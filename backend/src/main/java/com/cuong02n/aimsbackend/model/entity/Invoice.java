41960eacf92b2e29de959a9cd5a311fb1d669fc3
cuong02n <cuong02n@gmail.com>
2024-12-12 22:53:39 +0700

    Add category.

--- backend/src/main/java/com/cuong02n/aimsbackend/model/entity/Invoice.java
+++ backend/src/main/java/com/cuong02n/aimsbackend/model/entity/Invoice.java
@@ -1,7 +1,12 @@
 package com.cuong02n.aimsbackend.model.entity;
 
 
+import jakarta.persistence.Entity;
+
+//@Entity
 public class Invoice extends BaseEntity{
     private Order order;
+    private long shippingFee;
+    private long totalAmount;
 
 }
---
+++ backend/src/main/java/com/cuong02n/aimsbackend/model/entity/TransactionInfo.java
@@ -0,0 +1,14 @@
+//package com.cuong02n.aimsbackend.model.entity;
+//
+//import jakarta.persistence.Entity;
+//
+//import java.sql.Timestamp;
+//
+//@Entity
+//public class TransactionInfo {
+//    String transactionId;
+//    Timestamp transactionTime;
+//    String transactionMessage;
+//    private Invoice invoice;
+//
+//}
