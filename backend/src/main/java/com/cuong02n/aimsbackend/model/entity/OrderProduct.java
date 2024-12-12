95d2d03ead8a64cec91551924fc0bc29e77fd0eb
cuong02n <cuong02n@gmail.com>
2024-12-12 10:00:12 +0700

    Add Order Entity

--- backend/src/main/java/com/cuong02n/aimsbackend/model/entity/OrderProduct.java
+++ backend/src/main/java/com/cuong02n/aimsbackend/model/entity/OrderProduct.java
@@ -1,6 +1,30 @@
 package com.cuong02n.aimsbackend.model.entity;
 
 
-public class OrderProduct {
+import jakarta.persistence.*;
+import lombok.Data;
 
+@Entity
+public class OrderProduct {
+    @EmbeddedId
+    private OrderProductKey key;
+
+    @ManyToOne
+    @MapsId("productId")
+    @JoinColumn(name = "product_id")
+    private Product product;
+
+    @ManyToOne
+    @MapsId("orderId")
+    @JoinColumn(name = "order_id")
+    private Order order;
+    int quantity;
+    @Embeddable
+    @Data
+    private static class OrderProductKey {
+        @Column(name = "product_id")
+        private String productId;
+        @Column(name = "order_id")
+        private Long orderId;
+    }
 }
