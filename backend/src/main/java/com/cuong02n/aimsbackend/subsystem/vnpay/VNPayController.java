a2de5e80f9df0f1a6ce3eb2426fce5b5f322ea9b
cuong02n <cuong02n@gmail.com>
2025-01-06 22:21:45 +0700

    Update mail

--- backend/src/main/java/com/cuong02n/aimsbackend/subsystem/vnpay/VNPayController.java
+++ backend/src/main/java/com/cuong02n/aimsbackend/subsystem/vnpay/VNPayController.java
@@ -1,71 +1,80 @@
 package com.cuong02n.aimsbackend.subsystem.vnpay;
 
+import com.cuong02n.aimsbackend.model.entity.User;
 import com.cuong02n.aimsbackend.service.IEmailService;
-import com.cuong02n.aimsbackend.service.impl.EmailService;
+import jakarta.servlet.http.Cookie;
 import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.util.UriComponentsBuilder;
 
 import java.net.URI;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
+
 @CrossOrigin
 @RequestMapping("/api/vnpay")
 @Controller
 @RequiredArgsConstructor
 public class VNPayController {
     private final VNPayService vnPayService;
-    private final HttpServletRequest request;
+    private final HttpServletRequest httpServletRequest;
+    private final HttpServletResponse httpServletResponse;
     private final IEmailService emailService;
     @Value("${aims.frontend.base-url}")
     private String frontEndBaseUrl;
+
     @GetMapping("")
-    public String home(){
+    public String home() {
         return "index";
     }
+
 
     @PostMapping("/submitOrder")
     public ResponseEntity<String> submitOrder(@RequestParam("amount") Long orderTotal,
                                               @RequestParam("orderInfo") String orderInfo) {
-        //  UserDetails userDetails = (UserDetails) SecurityContextHolder.getContext().getAuthentication()
-        //   .getPrincipal();
-        //    User user= userService.getById(getUserId(userDetails))
-        //       .orElseThrow(()->new RuntimeException("user not found"));
-        //userService.createTransaction(user,orderTotal,Integer.parseInt(orderInfo));
-        String baseUrl = request.getScheme() + "://" + request.getServerName() + ":" + request.getServerPort();
+        User user = (User) httpServletRequest.getAttribute("user");
+
+        String baseUrl = httpServletRequest.getScheme() + "://" + httpServletRequest.getServerName() + ":" + httpServletRequest.getServerPort();
         //String vnpayUrl = vnPayService.createOrder(orderTotal, user.getId()+"_"+Integer.parseInt(orderInfo), baseUrl);
-        String vnpayUrl = vnPayService.createOrder(orderTotal, orderInfo, baseUrl);
+        String vnpayUrl = vnPayService.createOrder(user, orderTotal, orderInfo, baseUrl);
+        Cookie nameC = new Cookie("aims-user-name", user.getName());
+        nameC.setHttpOnly(true);
+        nameC.setSecure(false);
+        nameC.setPath("/");
+        nameC.setMaxAge(3600);
+        httpServletResponse.addCookie(nameC);
+        Cookie emailC = new Cookie("aims-user-email", user.getEmail());
+        emailC.setHttpOnly(true);
+        emailC.setSecure(false);
+        emailC.setPath("/");
+        emailC.setMaxAge(3600);
+        httpServletResponse.addCookie(emailC);
+        httpServletResponse.addHeader("Access-Control-Allow-Credentials","true");
         return ResponseEntity.ok(vnpayUrl);
     }
 
-    @GetMapping("/vnpay-status")
-    public ResponseEntity<?>  payStatus(HttpServletRequest request) {
-        int paymentStatus = vnPayService.orderReturn(request);
+    @GetMapping("/vnpay-status")// ?asdhasjd=&hajsdfhadf=d&email=achjshd&name=
+    public ResponseEntity<?> payStatus(
+            HttpServletRequest httpServletRequest,
+            @CookieValue("aims-user-name") String name,
+            @CookieValue("aims-user-email") String email
+    ) {
+        int paymentStatus = vnPayService.orderReturn(httpServletRequest);
 
-        String orderInfo = request.getParameter("vnp_OrderInfo");
-        String paymentTime = request.getParameter("vnp_PayDate");
-        String transactionId = request.getParameter("vnp_TransactionNo");
-        String totalPrice = request.getParameter("vnp_Amount").substring(0,request.getParameter("vnp_Amount").length()-2);
+        String orderInfo = httpServletRequest.getParameter("vnp_OrderInfo");
+        String paymentTime = httpServletRequest.getParameter("vnp_PayDate");
+        String transactionId = httpServletRequest.getParameter("vnp_TransactionNo");
+        String totalPrice = httpServletRequest.getParameter("vnp_Amount").substring(0, httpServletRequest.getParameter("vnp_Amount").length() - 2);
         // Định dạng lại thời gian thanh toán
         LocalDateTime paymentDateTime = LocalDateTime.parse(paymentTime, DateTimeFormatter.ofPattern("yyyyMMddHHmmss"));
         String formattedPaymentTime = paymentDateTime.format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss"));
-
-//        // Tạo đối tượng ModelAndView và chọn view để render
-//        ModelAndView modelAndView = new ModelAndView(paymentStatus == 1 ? "ordersuccess" : "orderfail");
-//
-//        // Thêm dữ liệu vào ModelAndView
-//        modelAndView.addObject("orderId", orderInfo);
-//        modelAndView.addObject("totalPrice", totalPrice);
-//        modelAndView.addObject("paymentTime", formattedPaymentTime);
-//        modelAndView.addObject("transactionId", transactionId);
 
 
         UriComponentsBuilder builder = UriComponentsBuilder.fromHttpUrl(frontEndBaseUrl)
@@ -79,12 +88,14 @@ public class VNPayController {
         HttpHeaders headers = new HttpHeaders();
         headers.setLocation(URI.create(builder.toUriString()));
 
-        emailService.sendMail(null,null,null,true);
+        if (paymentStatus == 1)
+            emailService.sendPaymentSuccessMail(email,
+                    name,
+                    builder.toUriString());
+
 
         return ResponseEntity.status(HttpStatus.FOUND)
                 .headers(headers)
                 .build();
-//
-//        return modelAndView;
     }
 }
